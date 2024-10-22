d77be049567ed2fc5acad4f7a8a1aafcba7beaa1
Kilian Lieret <kilian.lieret@posteo.de>
2024-10-22 10:35:05 -0400

    Do not test modal deployment in GHA

--- tests/test_modal_deployment.py
+++ tests/test_modal_deployment.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 import pytest
@@ -5,6 +6,7 @@ import pytest
 from swerex.deployment.modal import ModalDeployment
 
 
+@pytest.mark.skipif(os.getenv("GITHUB_ACTIONS") == "true", reason="Skipping modal tests in github actions")
 @pytest.mark.slow
 async def test_modal_deployment():
     dockerfile = Path(__file__).parent / "swe_rex_test.Dockerfile"
